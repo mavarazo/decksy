4a8569f3031aa4ec6dd02f995504a72536d3ebb4
Marco Niederberger <marco.niederberger@adcubum.com>
2019-08-10 13:19:12 +0200

    F init decksy

---
+++ src/main/java/com/decksy/DecksyApplication.java
@@ -0,0 +1,13 @@
+package com.decksy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecksyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecksyApplication.class, args);
+	}
+
+}
